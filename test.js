508b1022f14ae8360f3aa7f031d4fa828a46ae99
ghi3621 <ghi3621@gmail.com>
2024-01-09 20:41:41 +0900

    추가추가

--- test.js
+++ test.js
@@ -1,19 +1,24 @@
 
 
 function solution(sum) {
+    aaaaa
 }
 
 
 function solution(sub) {
+    bbbb
 }
 
 
 function solution(mul) {
+    ccccc
 }
 
 
 function solution(div) {
+    dddd
 }
 
 function solution(avg) {
+    eeee
 }
